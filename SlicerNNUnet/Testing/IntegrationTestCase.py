87095a492397f46f7879ea86e3c36829b3321426
Thibault Pelletier <thibault.pelletier@kitware.com>
2024-07-16 15:53:25 +0200

    TEST: Remove install test
    * Remove install test as it would sometimes corrupt local install

--- SlicerNNUnet/Testing/IntegrationTestCase.py
+++ SlicerNNUnet/Testing/IntegrationTestCase.py
@@ -12,10 +12,22 @@ class IntegrationTestCase(NNUNetTestCase):
         if not Parameter(modelPath=_dataFolderPath()).isValid():
             pytest.skip(f"Skipped : no nnUNet model available in {_dataFolderPath()}")
 
-        volume = load_test_CT_volume()
+        # Initialize widget
         widget = Widget(doShowInfoWindows=False)
+        widget.installLogic.doAskConfirmation = False
         widget.logic.errorOccurred.connect(print)
         widget.logic.progressInfo.connect(print)
+
+        # Install if needed and assert install was completed successfully
+        widget.ui.installButton.click()
+        self.assertTrue(widget.installLogic.isPackageInstalledAndCompatible("nnunetv2"))
+
+        # Check nnUNetV2 doesn't have torch as listed requirement
+        with open(widget.installLogic.packageMetaFilePath("nnunetv2"), "r") as f:
+            self.assertFalse("Requires-Dist: torch" in f.read())
+
+        # Set volume to segment and run segmentation
+        volume = load_test_CT_volume()
         widget.ui.inputSelector.setCurrentNode(volume)
         widget.ui.nnUNetModelPathEdit.setCurrentPath(_dataFolderPath())
 
@@ -23,23 +35,6 @@ class IntegrationTestCase(NNUNetTestCase):
         widget.logic.waitForSegmentationFinished()
         slicer.app.processEvents()
 
+        # Check segmentation was loaded
         segmentations = list(slicer.mrmlScene.GetNodesByClass("vtkMRMLSegmentationNode"))
         self.assertEqual(len(segmentations), 1)
-
-    def test_can_install_nn_unet_v2(self):
-        # Uninstall previous version if necessary
-        widget = Widget(doShowInfoWindows=False)
-        widget.installLogic.doAskConfirmation = False
-
-        packages_to_remove = ["torch", "nnunetv2", "pandas"]
-        for package in packages_to_remove:
-            slicer.util.pip_uninstall(package)
-
-        self.assertFalse(widget.installLogic.isPackageInstalledAndCompatible("nnunetv2"))
-        widget.ui.installButton.click()
-
-        self.assertTrue(widget.installLogic.isPackageInstalledAndCompatible("nnunetv2"))
-
-        # Check nnUNetV2 doesn't have torch as listed requirement
-        with open(widget.installLogic.packageMetaFilePath("nnunetv2"), "r") as f:
-            self.assertFalse("Requires-Dist: torch" in f.read())
